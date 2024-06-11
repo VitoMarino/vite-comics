3731d0f06590af3d148814b0c08f103d695a797a
VitoMarino <marino.vito2001@gmail.com>
2024-06-11 15:28:08 +0200

    Realizazzione cards

---
+++ src/components/AppMainParziale.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+    data() {
+        return {
+        }    
+    },
+    props: {
+        card: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <section>
+            
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
